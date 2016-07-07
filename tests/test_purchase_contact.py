1b6d3690aad9c10d6b98febf67d9732263577cf2
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-07 17:05:49 +0200

    Fix test

--- tests/test_purchase_contact.py
+++ tests/test_purchase_contact.py
@@ -2,23 +2,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('purchase_contact')
-
-    def test0005views(self):
-        'Test views'
-        test_view('purchase_contact')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'purchase_contact'
 
 
 def suite():
